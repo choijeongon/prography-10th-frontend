a7d4804b20eabdb1f1e724000860818ff363d37a
JeongOnChoi <chjo0330@gmail.com>
2025-02-16 21:30:49 +0900

    ➕update : Routes/Route 설정정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { HomeView } from "./views/HomeView";
+import { FunnelView } from "./views/FunnelView";
+import { Routes, Route } from "react-router";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+      <Route path="/funnel" element={<FunnelView />} />
+    </Routes>
+  );
 }
 
 export default App;
