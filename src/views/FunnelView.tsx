a423b9bd8f8518a0ae16d47cfcf50a674e3f46f3
JeongOnChoi <chjo0330@gmail.com>
2025-02-20 23:11:20 +0900

    update: FunnelView 및 PersonalInfo 컴포넌트 로직 업데이트

--- src/views/FunnelView.tsx
+++ src/views/FunnelView.tsx
@@ -4,6 +4,11 @@ import { BasicInfo } from "./funnel/BasicInfo";
 import { ApplyInfo } from "./funnel/ApplyInfo";
 import { Complete } from "./funnel/Complete";
 import { PGButton } from "../components/button/PGButton";
+import { StepCard } from "./funnel/step/StepCard";
+
+export interface UserInfo {
+  isAgree: boolean | null;
+}
 
 export const FunnelView = () => {
   // const [registerData, setRegisterData] = useState();
@@ -11,32 +16,65 @@ export const FunnelView = () => {
     "personalInfo" | "basicInfo" | "applyInfo" | "complete"
   >("personalInfo");
 
+  const [stepIndex, setStepIndex] = useState(1);
+
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    isAgree: null,
+  });
+
+  const checkIsDisabled = () => {
+    if (step === "personalInfo") {
+      return userInfo.isAgree === null;
+    }
+    return false;
+  };
+
+  const handleSetIsAgree = (isAgree: boolean | null) => {
+    setUserInfo({ ...userInfo, isAgree });
+  };
+
   const handleNextStep = () => {
     if (step === "personalInfo") {
       setStep("basicInfo");
+      setStepIndex(stepIndex + 1);
     } else if (step === "basicInfo") {
       setStep("applyInfo");
+      setStepIndex(stepIndex + 1);
     } else if (step === "applyInfo") {
       setStep("complete");
+      setStepIndex(stepIndex + 1);
     }
   };
 
   const handlePrevStep = () => {
     if (step === "basicInfo") {
       setStep("personalInfo");
+      setStepIndex(stepIndex - 1);
     } else if (step === "applyInfo") {
       setStep("basicInfo");
+      setStepIndex(stepIndex - 1);
     }
   };
 
   return (
     <>
-      {step === "personalInfo" && <PersonalInfo />}
+      <div className="w-full h-32 bg-white flex justify-center items-center rounded-lg">
+        <h1 className="text-3xl font-bold">Prography 10기 지원서</h1>
+      </div>
+      <div className="w-full h-24 bg-white flex justify-center items-center rounded-lg mt-8">
+        <StepCard step={stepIndex} />
+      </div>
+      {step === "personalInfo" && (
+        <PersonalInfo
+          isAgree={userInfo.isAgree}
+          setIsAgree={handleSetIsAgree}
+        />
+      )}
       {step === "basicInfo" && <BasicInfo />}
       {step === "applyInfo" && <ApplyInfo />}
       {step === "complete" && <Complete />}
       {step !== "complete" && (
-        <div className="w-full h-16 bg-white rounded-lg p-8 flex flex-row justify-between items-center">
+        <div className="w-full h-16 bg-white rounded-lg p-8 flex flex-row justify-between items-center mt-8">
           <PGButton
             label="뒤로"
             width="86px"
@@ -52,6 +90,7 @@ export const FunnelView = () => {
             height="36px"
             backgroundColor="#4696ff"
             fontColor="white"
+            disabled={checkIsDisabled()}
             onClick={handleNextStep}
           />
         </div>
--- src/views/funnel/PersonalInfo.tsx
+++ src/views/funnel/PersonalInfo.tsx
@@ -1,3 +1,44 @@
-export const PersonalInfo = () => {
-  return <div>PersonalInfo</div>;
+import { CardRadio } from "./card/CardRadio";
+import { CardTitle } from "./card/CardTitle";
+
+export interface PersonalInfoProps {
+  isAgree: boolean | null;
+  setIsAgree: (isAgree: boolean | null) => void;
+}
+
+export const PersonalInfo = ({ isAgree, setIsAgree }: PersonalInfoProps) => {
+  return (
+    <div className="w-full bg-white flex flex-col justify-start items-start rounded-lg mt-8 p-8">
+      <CardTitle
+        title="개인정보 수집 동의"
+        description="프로그라피 10기 지원을 위한 개인정보 수집에 대한 동의가 필요합니다"
+      />
+      <div className="w-full flex flex-col justify-start items-start p-4 border border-gray-200 rounded mt-2">
+        <div className="text-left font-semibold w-full">
+          <p>수집 목적: Prography 10기 리쿠르팅 과정 및 결과 안내</p>
+          <p>수집 항목: 이름, 이메일, 핸드폰 번호, 학교 정보 및 직장 정보</p>
+          <p>보유 및 이용 기간: 리쿠르팅 과정 종료일(3월 7일) 이후 파기</p>
+          <br />
+          <p>
+            개인정보 수집여부 동의 여부를 체크해주세요.{" "}
+            <span className="text-red-500">*</span>
+          </p>
+          <div className="flex flex-col justify-start items-start mt-3 gap-4">
+            <CardRadio
+              value={isAgree === true}
+              text="개인정보 수집 여부에 동의합니다"
+              name="personalInfo"
+              onChange={() => setIsAgree(true)}
+            />
+            <CardRadio
+              value={isAgree === false}
+              text="개인정보 수집 여부에 동의하지 않습니다"
+              name="personalInfo"
+              onChange={() => setIsAgree(false)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
