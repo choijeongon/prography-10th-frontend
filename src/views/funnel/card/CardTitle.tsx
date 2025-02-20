f376e4c00f3eb4ed5bc139bd8e012b51d49a379a
JeongOnChoi <chjo0330@gmail.com>
2025-02-20 23:10:59 +0900

    ➕update : CardTitle 컴포넌트 CSS 수정

--- src/views/funnel/card/CardTitle.tsx
+++ src/views/funnel/card/CardTitle.tsx
@@ -5,7 +5,7 @@ export interface CardTitleProps {
 
 export const CardTitle = ({ title, description }: CardTitleProps) => {
   return (
-    <div className="flex flex-col justify-start items-start gap-3">
+    <div className="flex flex-col justify-start items-start gap-3 w-full">
       <h2 className="text-2xl font-bold">{title}</h2>
       <div className="h-[2px] w-full bg-[#4696ff]"></div>
       <p className="text-sm text-gray-500">{description}</p>
